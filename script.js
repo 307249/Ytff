4f8d26397352b4682b679bcc8e22285e4cba176e
307249 <anterzead@gmail.com>
2025-07-15 19:59:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,23 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
+import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDVLILSHrL0jb8-c1PaxQlg8LmIVblSq4U",
-  authDomain: "drosak-2f9fe.firebaseapp.com",
-  databaseURL: "https://drosak-2f9fe-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "drosak-2f9fe",
-  storageBucket: "drosak-2f9fe.appspot.com",
-  messagingSenderId: "23383414815",
-  appId: "1:23383414815:web:0dfd6a04f92b5e87099d2a"
+  databaseURL: "https://drosak-v2-default-rtdb.europe-west1.firebasedatabase.app/"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
 
+// زر التفعيل/إلغاء التفعيل
 document.getElementById('lockToggle').addEventListener('change', function () {
   const isLocked = this.checked;
   set(ref(db, 'appSettings/lockEnabled'), isLocked);
 });
 
+// توليد المفتاح
 function generateKey(type) {
   const key = Math.floor(1000000 + Math.random() * 9000000).toString();
-  let expiry = '';
+  let expiry = 0;
 
   if (type === '2h') {
     expiry = Date.now() + 2 * 60 * 60 * 1000;
